ebfa5501dc2e8c7fa2fb188c1718937f30ec32da
Anmol Baldawa <anmol.baldawa@gmail.com>
2020-09-12 15:06:48 +0530

    Auto updating digital clock using tkinter.

--- Digital Clock/DigitalClock.py
+++ Digital Clock/DigitalClock.py
@@ -3,7 +3,7 @@ import tkinter
 
 tkObject = tkinter.Tk()
 tkObject.title("Digital Clock")
-tkObject.geometry("500x200+0+0")
+tkObject.geometry("500x200+0+0")  # Size and position of the pop-up window
 tkObject.resizable(True, True)
 label = tkinter.Label(master=tkObject, background='yellow', font=('Chiller', 30, 'bold'))
 label.grid(row=0, column=1)
@@ -11,7 +11,7 @@ label.grid(row=0, column=1)
 
 def func():
     label.config(text="Time is " + time.strftime("%I:%M:%S %p"))
-    label.after(ms=500, func=func)
+    label.after(ms=500, func=func)  # Change the ms(milliseconds) to vary the update time of clock
 
 
 func()
